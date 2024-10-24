2e6d367ae62ccf5fd12b843c66312c4e362230e5
ColbyD <colbydomingos@my.whccd.edu>
2024-10-23 20:08:59 -0700

    Finished 3rd Javascript File

--- script3.js
+++ script3.js
@@ -9,3 +9,19 @@
 //  # # # #
 // # # # #
 //  # # # #
+
+let size = 8; // Changing this will make it larger or smaller
+let board = " "
+
+for (let row = 0; row < size; row++){
+    for (let col = 0; col < size; col++){
+        if ((row + col) % 2 === 0){
+            board += " ";
+        } else {
+            board += "#";
+        }
+    }
+    board += "\n"; // needs to start a new line for the board to continue
+}
+
+console.log(board);
